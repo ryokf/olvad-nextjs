305d015923d1de8e527678b6a6ea930daaaff085
ryokf <ryokhrisnaf@gmail.com>
2025-01-24 10:52:47 +0700

    Add dark mode support and Navbar component; update styles and dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
+import { content, plugin } from "flowbite-react/tailwind";
 
+/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class", '[data-theme="dark"]'],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    content(),
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          'DEFAULT': '#675D50',
+        }
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin()
+  ],
 } satisfies Config;
